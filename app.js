6b49e8412df57edc7c1ddcb6a0c0ee9dd8b07772
Xiaojie LI <leo.li.ca@outlook.com>
2021-06-20 16:02:10 -0400

    finish transactions process

--- app.js
+++ app.js
@@ -3,16 +3,78 @@ const fetchData = async url => {
     return await response.json()
 }
 
+const calculateNetWorth = (accountBalances, rate) => {
+    return accountBalances.cad + accountBalances.btc * rate.BTC_CAD + accountBalances.eth * rate.ETH_CAD
+}
+
+const processTransactions = (transactions, rate) => {
+    transactions = transactions.reverse()
+    let accountBalances = {
+        cad: 0,
+        btc: 0,
+        eth: 0,
+    }
+    let transactionsOrderByDate = {}
+    let netWorthOrderByDate = {}
+
+    for (transaction of transactions) {
+        const date = transaction.createdAt.substring(0, 10)
+        if (!transactionsOrderByDate.hasOwnProperty(date)) {
+            transactionsOrderByDate[date] = [];
+        }
+        transactionsOrderByDate[date].push(transaction);
+        processTransaction(accountBalances, transaction)
+        netWorthOrderByDate[date] = calculateNetWorth(accountBalances, rate)
+    }
+
+    return {
+        transactionsOrderByDate,
+        netWorthOrderByDate,
+    }
+}
+
+const processTransaction = (accountBalances, transaction) => {
+    const account = transaction.currency.toLowerCase()
+    const amount = transaction.amount
+
+    if (transaction.direction === 'credit') {
+        accountBalances[account] += amount
+
+        // TODO: debug
+        if (account === 'btc') {
+            console.log(transaction.createdAt + ' / BTC credit+++++: ' + amount + ', BTC total: ' + accountBalances.btc);
+        }
+
+    } else if (transaction.direction === 'debit') {
+        accountBalances[account] -= amount
+
+        // TODO: debug
+        if (account === 'btc') {
+            console.log(transaction.createdAt + ' / BTC debit----- ' + amount + ', BTC total: ' + accountBalances.btc);
+        }
+    } else {
+        const fromAccount = transaction.from.currency.toLowerCase()
+        const fromAmount = transaction.from.amount
+        const toAccount = transaction.to.currency.toLowerCase()
+        const toAmount = transaction.to.amount
+        accountBalances[fromAccount] -= fromAmount
+        accountBalances[toAccount] += toAmount
+    }
+}
+
+const generateDrewPoints = netWorthOrderByDate => {
+    // TODO: 
+}
 
 
 (async () => {
     const CONST_RATE = await fetchData('./data/const_rate.json')
     const CONST_TRANSACTIONS = await fetchData('./data/transaction_history.json')
-    
+
+    const {transactionsOrderByDate, netWorthOrderByDate} = processTransactions(CONST_TRANSACTIONS, CONST_RATE)
 
     // TODO: debug
-    console.log({CONST_RATE, CONST_TRANSACTIONS})
-
+    console.log({transactionsOrderByDate, netWorthOrderByDate})
 })();
 
 
